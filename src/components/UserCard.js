46153d8917d724f0ceddc11be7e47ac98cc17c42
LanaWeb <lana@pro.local>
2020-05-06 13:20:44 -0700

    UserList and UserCard as functional components

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,16 +1,30 @@
 import React from 'react';
 
-class UserCard extends React.Component{
+// class UserCard extends React.Component{
 
-    render(){
-        return(
-            <div className="user-card">
-                <img src={this.props.img} alt="a cat" height="242" width="242"></img>
-                <p>{this.props.name}</p>
-            </div>
-        )
+//     render(){
+//         return(
+//             <div className="user-card">
+//                 <img src={this.props.img} alt="a cat" height="242" width="242"></img>
+//                 <p>{this.props.name}</p>
+//             </div>
+//         )
 
-    }
+//     }
+// }
+
+// export default UserCard;
+
+
+function UserCard(props){ //functional component usualy used when there is no need in state
+
+    return(
+        <div className="user-card">
+            <img src={props.img} alt="a cat" height="242" width="242"></img>
+            <p>{props.name}</p>
+        </div>
+    )
+
 }
 
 export default UserCard;
--- src/components/UserList.js
+++ src/components/UserList.js
@@ -2,23 +2,22 @@ import React from 'react';
 import UserCard from './UserCard';
 
 
-class UserList extends React.Component {
+function UserList() {
+    let users = [
+        { img: "https://www.pbs.org/wnet/nature/files/2018/09/Nature-Thumbnail-FishingCat-MM-512x288.jpg", name: "Dilly" },
+        { img: "https://lh3.googleusercontent.com/proxy/UCA5Eh-WjhFBDA-tLX5zdtzvFDeVtRqzwpbKJmfnTeividaIYSlf0-t_9XxpYPOn_cqduiLVsG-G78WkVFLFPWT9UBA7crbS1JwYHomFHT9RONHTm08", name: "Subby" },
+        { img: "https://assets-natgeotv.fnghub.com/Shows/16598.jpg", name: "Kiky" },
+        { img: "https://assets-natgeotv.fnghub.com/Shows/17174.jpg", name: "Milky" },
+        { img: "https://wallup.net/wp-content/uploads/2016/01/19871-nature-cat.jpg", name: "Lilliy" }
+    ]
 
-    render() {
-        let users = [
-            { img: "https://www.pbs.org/wnet/nature/files/2018/09/Nature-Thumbnail-FishingCat-MM-512x288.jpg", name: "Dilly" },
-            { img: "https://lh3.googleusercontent.com/proxy/UCA5Eh-WjhFBDA-tLX5zdtzvFDeVtRqzwpbKJmfnTeividaIYSlf0-t_9XxpYPOn_cqduiLVsG-G78WkVFLFPWT9UBA7crbS1JwYHomFHT9RONHTm08", name: "Subby" },
-            { img: "https://assets-natgeotv.fnghub.com/Shows/16598.jpg", name: "Kiky" },
-            { img: "https://assets-natgeotv.fnghub.com/Shows/17174.jpg", name: "Milky" },
-            { img: "https://wallup.net/wp-content/uploads/2016/01/19871-nature-cat.jpg", name: "Lilliy" }
-        ]
-
-        return (
-            <div className="user-list">
-                {users.map(user => <UserCard img={user.img} name={user.name} />)}
-            </div>
-        )
-    }
+    return (
+        <div className="user-list">
+            {users.map(user => <UserCard img={user.img} name={user.name} />)}
+        </div>
+    )
 }
 
 export default UserList;
+
+
